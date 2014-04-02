af8c28eeb6d0346933a364d41a70dff0fa90d974
Angela Yen & Anne Jhetson <student@epicodus.com>
2014-04-02 14:04:23 -0700

    complete CRUD/L methods for lesson model

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -6,7 +6,7 @@ class LessonsController < ApplicationController
   end
 
   def new
-    @lessons = Lesson.all
+    @lessons = Lesson.order(:number)
     render('lessons/new.html.erb')
   end
 
@@ -18,5 +18,21 @@ class LessonsController < ApplicationController
   def show
     @lesson = Lesson.find(params[:id])
     render('lessons/show.html.erb')
+  end
+
+  def edit
+    @lesson = Lesson.find(params[:id])
+    render('lessons/edit.html.erb')
+  end
+
+  def update
+    @lesson = Lesson.find(params[:id])
+    @lesson.update(params[:lesson])
+    redirect_to("/lessons/#{@lesson.id}")
+  end
+
+  def destroy
+    @lesson = Lesson.find(params[:id]).destroy
+    redirect_to("/lessons")
   end
 end
